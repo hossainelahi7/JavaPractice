6692f98b6e46128d4b299796bd47ce79d5e79411
hossainelahi <hossain.elahi@live.com>
2022-07-13 00:14:33 -0700

    Completed

--- src/main/java/com/practice/graph/MinArea.java
+++ src/main/java/com/practice/graph/MinArea.java
@@ -7,11 +7,33 @@ import java.util.List;
 
 public class MinArea {
 //https://leetcode.com/explore/interview/card/google/66/others-4/3105/
-    List<Point> visited = new ArrayList<>();
-
-
 
     public int minAreaRect(int[][] points) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] p : points) {
+            if (!map.containsKey(p[0])) {
+                map.put(p[0], new HashSet<>());
+            }
+            map.get(p[0]).add(p[1]);
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < points.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if (points[i][0] == points[j][0] || points[i][1] == points[j][1]) { // if have the same x or y
+                    continue;
+                }
+                if (map.get(points[i][0]).contains(points[j][1]) && map.get(points[j][0]).contains(points[i][1])) { // find other two points
+                    int current = Math.abs((points[i][0] - points[j][0])*(points[i][1] - points[j][1]));
+                    if(min > current)
+                        min = current;
+                }
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+
+
+/*    public int minAreaRect2(int[][] points) {
         // create a map x -> all y and y -> all x; Sort all keys and also the values.
         if(points.length<=1){
             return 0;
@@ -40,7 +62,7 @@ public class MinArea {
             Queue<Point> next = new ArrayDeque<>();
             next.addAll(getNeibours(key, xValues.get(key)));
             while (!next.isEmpty()){
-                Point point1 =
+                Point point1; //TODO
             }
         }
 
@@ -58,5 +80,5 @@ public class MinArea {
             }
         });
         return pointList;
-    }
+    }*/
 }
